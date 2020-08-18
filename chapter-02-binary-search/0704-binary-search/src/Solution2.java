279eab07a9cc22888dca65e50aaa5afd102f2e04
liweiwei1419 <liweiwei1419@gmail.com>
2020-08-18 23:41:27 +0800

    添加了二分查找章节的代码。

---
+++ chapter-02-binary-search/0704-binary-search/src/Solution2.java
@@ -0,0 +1,28 @@
+public class Solution2 {
+
+    // 「力扣」第 704 题：二分查找
+    // 地址：https://leetcode-cn.com/problems/binary-search/
+
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+
+        int left = 0;
+        int right = len - 1;
+        // 目标元素可能存在在区间 [left, right]
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else {
+                // 下一轮搜索区间是 [left, mid]
+                right = mid;
+            }
+        }
+
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+}
