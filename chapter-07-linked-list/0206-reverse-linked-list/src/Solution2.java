85f0ac0db14730942be7b0102702a352937cff2b
liweiwei1419 <liweiwei1419@gmail.com>
2020-08-18 23:47:55 +0800

    添加了「链表」的代码。

---
+++ chapter-07-linked-list/0206-reverse-linked-list/src/Solution2.java
@@ -0,0 +1,16 @@
+public class Solution2 {
+
+    public ListNode reverseList(ListNode head) {
+        // 特判
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode nextNode = head.next;
+        ListNode newNode = reverseList(nextNode);
+        nextNode.next = head;
+        // 这里不要忘记切断引用，否则会出现错误：Error - Found cycle in the ListNode
+        head.next = null;
+        return newNode;
+    }
+}
